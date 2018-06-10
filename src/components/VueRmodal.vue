793ef8446fbbbd1be2a42a8712dd1e8250bee93b
Chanwit Piromplad <kingkong2103@gmail.com>
2018-06-10 21:19:36 +0700

    WIP test use rmodal

---
+++ src/components/VueRmodal.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div id="modal" class="modal">
+      <div class="modal-dialog animated">
+        <div class="modal-content">
+          <form class="form-horizontal" method="get">
+            <div class="modal-header">
+              <strong>Yep, I'm a dialog...</strong>
+            </div>
+
+            <div class="modal-body">
+              <div class="form-group">
+                <label for="dummyText" class="control-label col-xs-4">Dummy text</label>
+                <div class="input-group col-xs-7">
+                  <input type="text" name="dummyText" id="dummyText" class="form-control" />
+                </div>
+              </div>
+            </div>
+
+            <div class="modal-footer">
+              <button class="btn btn-default" type="button" onclick="modal.close();">Cancel</button>
+              <button class="btn btn-primary" type="submit" onclick="modal.close();">Save</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <a href="#" id="showModal" class="btn btn-success">Show modal</a>
+  </div>
+</template>
+
+<script>
+import RModal from 'rmodal/dist/rmodal.js';
+  window.onload = function() {
+    var modal = new RModal(document.getElementById('modal'), {
+        //content: 'Abracadabra'
+        beforeOpen: function(next) {
+            console.log('beforeOpen');
+            next();
+        }
+        , afterOpen: function() {
+            console.log('opened');
+        }
+
+        , beforeClose: function(next) {
+            console.log('beforeClose');
+            next();
+        }
+        , afterClose: function() {
+            console.log('closed');
+        }
+        // , bodyClass: 'modal-open'
+        // , dialogClass: 'modal-dialog'
+        // , dialogOpenClass: 'animated fadeIn'
+        // , dialogCloseClass: 'animated fadeOut'
+
+        // , focus: true
+        // , focusElements: ['input.form-control', 'textarea', 'button.btn-primary']
+
+        // , escapeClose: true
+    });
+
+    document.addEventListener('keydown', function(ev) {
+        modal.keydown(ev);
+    }, false);
+
+    document.getElementById('showModal').addEventListener("click", function(ev) {
+        ev.preventDefault();
+        modal.open();
+    }, false);
+
+    window.modal = modal;
+  }
+</script>
