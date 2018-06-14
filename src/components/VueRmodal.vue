fb353c31c3cb1bbd95f59b5cf68535cee6bc0c10
Chanwit Piromplad <kingkong2103@gmail.com>
2018-06-14 23:21:54 +0700

    use in directive

--- src/components/VueRmodal.vue
+++ src/components/VueRmodal.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div id="modal" class="modal">
+    <div id="modal" class="modal" v-rmodal="{setting}">
       <div class="modal-dialog animated">
         <div class="modal-content">
           <form class="form-horizontal" method="get">
@@ -26,7 +26,7 @@
       </div>
     </div>
 
-    <a href="#" id="showModal" class="btn btn-success">Show modal</a>
+    <a @click="showModal" id="showModal" class="btn btn-success">Show modal</a>
   </div>
 </template>
 
@@ -34,45 +34,43 @@
 import RModal from 'rmodal/src/rmodal.js';
 export default {
   name: 'rmodal',
+  directives: {
+    rmodal: {
+      bind (el, {value}) {
+        console.log()
+        const setting = value.setting
+        var modal = new RModal(el, setting)
+        window.modal = modal;
+      }
+    }
+  },
+  methods: {
+    showModal() {
+      window.modal.open()
+    }
+  },
+  computed: {
+    setting () {
+      return {
+          beforeOpen: function(next) {
+              console.log('beforeOpen')
+              next()
+          },
+          afterOpen: function() {
+              console.log('opened');
+          },
+          beforeClose: function(next) {
+              console.log('beforeClose');
+              next();
+          },
+          afterClose: function() {
+              console.log('closed');
+          }
+        }
+    }
+  },
   mounted () {
-    var modal = new RModal(document.getElementById('modal'), {
-        //content: 'Abracadabra'
-        beforeOpen: function(next) {
-            console.log('beforeOpen');
-            next();
-        }
-        , afterOpen: function() {
-            console.log('opened');
-        }
-
-        , beforeClose: function(next) {
-            console.log('beforeClose');
-            next();
-        }
-        , afterClose: function() {
-            console.log('closed');
-        }
-        // , bodyClass: 'modal-open'
-        // , dialogClass: 'modal-dialog'
-        // , dialogOpenClass: 'animated fadeIn'
-        // , dialogCloseClass: 'animated fadeOut'
-
-        // , focus: true
-        // , focusElements: ['input.form-control', 'textarea', 'button.btn-primary']
-
-        // , escapeClose: true
-    });
-
-    document.addEventListener('keydown', function(ev) {
-        modal.keydown(ev);
-    }, false);
-
-    document.getElementById('showModal').addEventListener("click", function(ev) {
-        ev.preventDefault();
-        modal.open();
-    }, false);
-
-    window.modal = modal;
+    var el = document.getElementById('modal')
   }
 }
 </script>
